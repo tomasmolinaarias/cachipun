90c738e15f9241616272d94872d275d91c773e46
tomasmolinaarias <tomasmolinaarias12@gmail.com>
2021-10-08 00:05:01 -0300

    casi listo falta arregla cosas

---
+++ assets/js/app.js
@@ -0,0 +1,81 @@
+// selecion de partidas
+let partidas = parseInt (prompt ("cuantas partidas quieres jugar "));
+
+let jugadaPC = "";
+let jugada ="";
+let resumen = "";
+
+function eleccionMaquina(jugadaPC){
+    let numero = Math.floor(Math.random()*3);
+    switch(numero){
+        case `0`:
+            jugadaPC = "piedra"
+        break;
+        case `1`:
+            jugadaPC = "tijera"
+        break;
+        case `2`:
+            jugadaPC = "papel"
+        break;    
+    };
+    return jugadaPC;
+};
+
+//funcion para el resulatado del juego
+function cachipun (jugada, jugadaPC){
+    if(jugadaPC === jugada){
+        resultado = "empate";
+    } //winner
+    else if(jugadaPC === "tijera" && jugada === "piedra"){
+        resultado = "victoria";
+    }
+    else  if(jugadaPC === "papel" && jugada === "tijera"){
+        resultado = "victoria";
+    }
+    else if(jugadaPC === "piedra" && jugada === "papel"){
+        resultado = "victoria";
+    } //gameover
+    else if(jugadaPC === "tijera" && jugada === "papel"){
+        resultado = "derrota";
+    }
+    else if(jugadaPC === "piedra" && jugada === "tijera"){
+        resultado = "derrota";
+    }
+    else if(jugadaPC === "papel" && jugada === "piedra"){
+        resultado = "derrota";
+    }
+    else {
+        resultado = "error";
+    }
+    return resultado;
+};
+for(i=0; i < partidas; i++) {
+    jugada = prompt (`¿cual elijiras tijera, piedra o papel?`,`piedra`).toLowerCase();
+    jugadaPC = eleccionMaquina (jugadaPC)
+    resumen = cachipun(jugada, jugadaPC)
+    switch(resumen){
+        case "victoria":
+             alert ("felicidades has ganado contra la consola 😸")
+             console.log("felicidades")
+        break;
+        case "derrota":
+            alert ("lo siento perdiste contra la consola 😿")
+            console.log("sorry para la otra")
+        break;
+        case "empate":
+            alert ("cerca fue un empate 🙀")
+            console.log("cerca")
+        break;
+        default:
+             alert("lo siento pero no escribiste lo que te pedimos")
+        break;
+    };
+    
+};
+
+
+
+
+
+
+
