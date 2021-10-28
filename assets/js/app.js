4259954c0f6718fc9b20e0361b5a90dff3cb41b2
tomasmolinaarias <tomasmolinaarias12@gmail.com>
2021-10-28 18:32:14 -0300

    funciona

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,35 +1,40 @@
-const boton = document.getElementById('btn');
+// let boton = document.getElementById('btn');
+let jugando = document.getElementById("jugada")
 // let random = Math.floor(Math.random()*(4-1))+1;
 // console.log(random)
-boton.addEventListener('click', function resultado(jugando) {
+document.getElementById('btn').addEventListener('click', function () {
+    console.log(jugando.value)
     let random = Math.floor(Math.random() * (4 - 1)) + 1;
-
-    let jugadon = document.getElementById("jugada").value
-    if (jugadon === random) {
+    console.log(random)
+    if (jugando.value === random) {
         alert = ("empate")
-    } else if (jugando == 1 && random === 2) {
+        console.log('empate')
+    } else if (jugando.value == 1 && random === 2) {
+        alert("perdiste intenta otra ves")
+        console.log('perdiste')
+    } else if (jugando.value == 2 && random === 3) {
 
         alert("perdiste intenta otra ves")
-    } else if (jugando == 2 && random === 3) {
+        console.log('perdiste')
+    } else if (jugando.value == 3 && random === 1) {
 
         alert("perdiste intenta otra ves")
-    } else if (jugando == 3 && random === 1) {
-
-        alert("perdiste intenta otra ves")
-    } else if (jugando == 1 && random === 3) {
+        console.log('perdiste')
+    } else if (jugando.value == 1 && random === 3) {
 
         alert("ganaste quieres jugar de nuevo")
-    } else if (jugando == 2 && random === 1) {
+        console.log('ganaste')
+    } else if (jugando.value == 2 && random === 1) {
 
         alert("ganaste quieres jugar de nuevo")
-    } else if (jugando == 3 && random === 2) {
+        console.log('ganaste')
+    } else if (jugando.value == 3 && random === 2) {
 
         alert("ganaste quieres jugar de nuevo")
+        console.log('ganaste')
     } else {
 
         alert("porfavor ingresa un numero del 1 al 3")
     }
-    return [
-        [resultado]
-    ]
+
 })
